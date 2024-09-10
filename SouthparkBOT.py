5545bc8406ef2c931d05ab624c359ca030f8ac71
gyglgy <94980456+gyglgy@users.noreply.github.com>
2024-09-10 20:31:48 +0300

    Update SouthparkBOT.py

--- SouthparkBOT.py
+++ SouthparkBOT.py
@@ -1,9 +1,20 @@
+import logging
 from telethon import TelegramClient, events
 from dotenv import load_dotenv, set_key
 import asyncio
 import random
 import os
 import datetime
+
+# Настраиваем логирование
+logging.basicConfig(
+    level=logging.DEBUG,  # Уровень логов (INFO, DEBUG, WARNING, ERROR, CRITICAL)
+    format='%(asctime)s - %(levelname)s - %(message)s',  # Формат логов
+    handlers=[
+        logging.FileHandler("telegram_bot.log"),  # Логи будут записываться в файл
+        logging.StreamHandler()  # Логи будут выводиться в консоль
+    ]
+)
 
 load_dotenv()
 # Вставьте сюда свои данные
@@ -25,28 +36,25 @@ client = TelegramClient(
     app_version='7.2.1'  # Версия приложения
 )
 
-
 def generate_random_series():
     """Генерирует случайную серию для текущей даты."""
     today = datetime.datetime.now().strftime('%Y%m%d')
     random_number = random.randint(1000, 9999)
     return f"{today}-{random_number}"
 
-
 async def delete_previous_message(channel, previous_message_id):
     """Удаляет предыдущее сообщение по его ID."""
     try:
         if previous_message_id:
             await client.delete_messages(channel, int(previous_message_id))
-            print(f"Предыдущее сообщение с ID {previous_message_id} удалено.")
+            logging.info(f"Предыдущее сообщение с ID {previous_message_id} удалено.")
     except Exception as e:
-        print(f"Ошибка при удалении сообщения с ID {previous_message_id}: {e}")
-
+        logging.error(f"Ошибка при удалении сообщения с ID {previous_message_id}: {e}")
 
 async def main():
     try:
         await client.start(phone_number)
-        print("Клиент запущен.")
+        logging.info("Клиент запущен.")
 
         # Получаем последний сохраненный ID сообщения для двух каналов из .env
         previous_message_id_channel_one = os.getenv('last_message_id_channel_one')
@@ -55,7 +63,7 @@ async def main():
         while True:
             # Получаем последние 1000 сообщений из первого канала
             messages = await client.get_messages(channels[0], limit=1000)
-            print(f"Получено сообщений: {len(messages)}")
+            logging.info(f"Получено сообщений: {len(messages)}")
 
             # Фильтруем сообщения для нахождения видео
             video_messages = [msg for msg in messages if msg.video]
@@ -63,19 +71,19 @@ async def main():
             if video_messages:
                 # Рандомизация среди сообщений с видео
                 random_video = random.choice(video_messages)
-                print(f"Выбрано видео ID: {random_video.id}")
+                logging.info(f"Выбрано видео ID: {random_video.id}")
 
                 # Получаем текстовое содержимое сообщения
                 message_text = random_video.text or ""
-                print(f"Текст сообщения: {message_text}")
+                logging.info(f"Текст сообщения: {message_text}")
 
                 # Генерируем случайную серию
                 random_series = generate_random_series()
-                print(f"Случайная серия: {random_series}")
+                logging.info(f"Случайная серия: {random_series}")
 
                 # Формируем тексты для двух сообщений
-                new_message_text_one = f"{message_text}\n\nСлучайная серия на сегодня\n\n[Все серии южного парка](https://t.me/southsparkvse)"
-                new_message_text_two = f"{message_text}\n\nСлучайная серия на сегодня\n\n[Наш другой канал с фильмами и сериалами](https://t.me/apelsinovypodval)"
+                new_message_text_one = f"{message_text}\n\nСлучайная серия на сегодня\n\n[Наш другой канал с фильмами и сериалами](https://t.me/apelsinovypodval)"
+                new_message_text_two = f"{message_text}\n\nСлучайная серия на сегодня\n\n[Все серии южного парка](https://t.me/southsparkvse)"
 
                 # Отправляем сообщение в первый канал и удаляем предыдущее
                 await delete_previous_message(channels[0], previous_message_id_channel_one)
@@ -84,10 +92,10 @@ async def main():
                     new_message_text_one,  # Новый текст сообщения
                     file=random_video.video  # Пересылаем видео
                 )
-                print(f"Сообщение с видео и текстом переслано в {channels[0]}.")
+                logging.info(f"Сообщение с видео и текстом переслано в {channels[0]}.")
                 # Сохраняем ID отправленного сообщения в .env для первого канала
                 set_key('.env', 'last_message_id_channel_one', str(sent_message_one.id))
-                print(f"ID отправленного сообщения {sent_message_one.id} сохранен в .env для первого канала.")
+                logging.info(f"ID отправленного сообщения {sent_message_one.id} сохранен в .env для первого канала.")
 
                 # Отправляем сообщение во второй канал (супергруппа с топиком) и удаляем предыдущее
                 await delete_previous_message(channels[1][0], previous_message_id_channel_two)
@@ -97,22 +105,21 @@ async def main():
                     file=random_video.video,  # Пересылаем видео
                     reply_to=channels[1][1]  # Отправляем в конкретный топик
                 )
-                print(f"Сообщение с видео и текстом переслано в топик {channels[1][1]} супергруппы {channels[1][0]}.")
+                logging.info(f"Сообщение с видео и текстом переслано в топик {channels[1][1]} супергруппы {channels[1][0]}.")
                 # Сохраняем ID отправленного сообщения в .env для второго канала
                 set_key('.env', 'last_message_id_channel_two', str(sent_message_two.id))
-                print(f"ID отправленного сообщения {sent_message_two.id} сохранен в .env для второго канала.")
+                logging.info(f"ID отправленного сообщения {sent_message_two.id} сохранен в .env для второго канала.")
 
                 break  # Выход из цикла после успешной пересылки
             else:
-                print("Видео не найдено, пробуем снова.")
+                logging.info("Видео не найдено, пробуем снова.")
                 await asyncio.sleep(10)  # Ожидание перед новой попыткой
 
     except Exception as e:
-        print(f"Произошла ошибка: {e}")
+        logging.error(f"Произошла ошибка: {e}")
 
     finally:
         await client.run_until_disconnected()
-
 
 if __name__ == '__main__':
     asyncio.run(main())
